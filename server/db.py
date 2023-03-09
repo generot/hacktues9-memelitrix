e55f38475efcffd6e16690b09c96cc056800331c
AlexOgn <alexognyanov@abv.bg>
2023-03-09 17:34:09 +0200

    opravih lainata, kur za komitko

--- server/db.py
+++ server/db.py
@@ -3,10 +3,7 @@ import hashlib
 import os
 from bson import ObjectId
 from dotenv import load_dotenv
-<<<<<<< HEAD
 import json
-=======
->>>>>>> 05fcb1cfc319d7c78797c18676d155fa862fb8b8
 
 load_dotenv()
 uri = os.environ["MONGODB_URI"]
@@ -14,34 +11,21 @@ uri = os.environ["MONGODB_URI"]
 client = pymongo.MongoClient(uri)
 db = client["data"]
 users = db["users"]
-<<<<<<< HEAD
 break_ins = db["break_ins"]
 devices = db["devices"]
 
 
 def add_user(username, password):
     # check if user exists
-=======
-points = db["points"]
-devices = db["devices"]
-
-def add_user(username, password):
-    #check if user exists
->>>>>>> 05fcb1cfc319d7c78797c18676d155fa862fb8b8
     for i in users.find({}):
         if i["name"] == username:
             return {"code": 400, "message": "User already exists"}
 
-<<<<<<< HEAD
     # password hashing
-=======
-    #password hashing
->>>>>>> 05fcb1cfc319d7c78797c18676d155fa862fb8b8
     password = password.encode()
     salt = 32*b'\x00'
     password_hash = hashlib.pbkdf2_hmac('sha256', password, salt, 100000)
 
-<<<<<<< HEAD
     # insert user
     _id = users.insert_one(
         {'name': username, 'password': password_hash, 'device_ids': None})
@@ -49,39 +33,22 @@ def add_user(username, password):
     return {"code": 200, "message": "User registered successfully", "id": str(_id.inserted_id)}
 
 
-=======
-    #insert user
-    _id = users.insert_one({'name': username, 'password': password_hash, 'device_ids': None})
-
-    return {"code": 200, "message": "User registered successfully", "id": str(_id.inserted_id)}
-
->>>>>>> 05fcb1cfc319d7c78797c18676d155fa862fb8b8
 def check_user(username, password):
     for i in users.find({}):
         if i["name"] == username:
             password = password.encode()
             salt = 32*b'\x00'
-<<<<<<< HEAD
             password_hash = hashlib.pbkdf2_hmac(
                 'sha256', password, salt, 100000)
-=======
-            password_hash = hashlib.pbkdf2_hmac('sha256', password, salt, 100000)
->>>>>>> 05fcb1cfc319d7c78797c18676d155fa862fb8b8
 
             if i["password"] == password_hash:
                 return {"code": 200, "message": "User logged in successfully", "username": i["name"], "id": str(i["_id"])}
             else:
                 return {"code": 400, "message": "Username or password is incorrect"}
-<<<<<<< HEAD
 
     return {"code": 400, "message": "Username or password is incorrect"}
 
 
-=======
-    
-    return {"code": 400, "message": "Username or password is incorrect"}
-
->>>>>>> 05fcb1cfc319d7c78797c18676d155fa862fb8b8
 def fetch_users():
     cursor = users.find({})
     all_users = []
@@ -90,16 +57,10 @@ def fetch_users():
         i["_id"] = None
         i["password"] = None
         all_users.append(i)
-<<<<<<< HEAD
 
     return {"code": 200, "message": "STATUS OK", "users": all_users}
 
 
-=======
-        
-    return {"code": 200, "message": "STATUS OK", "users": all_users}
-
->>>>>>> 05fcb1cfc319d7c78797c18676d155fa862fb8b8
 def verify_user_db(username):
     schema = {"name": username}
     user = users.find_one(schema)
@@ -109,33 +70,22 @@ def verify_user_db(username):
 
     return {"code": 200, "message": "STATUS OK", "exists": False}
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 05fcb1cfc319d7c78797c18676d155fa862fb8b8
 def add_device(id, owner_id, lat, lon):
     for i in devices.find({}):
         if i["id"] == id:
             return {"code": 400, "message": "Device already registered"}
 
-<<<<<<< HEAD
     devices.insert_one(
         {'id': id, 'owner_id': owner_id, 'lat': lat, 'lon': lon})
 
     return {"code": 200, "message": "Device registered successfully"}
 
 
-=======
-    devices.insert_one({'id': id, 'owner_id': owner_id, 'lat': lat, 'lon': lon})
-
-    return {"code": 200, "message": "Device registered successfully"}
-
->>>>>>> 05fcb1cfc319d7c78797c18676d155fa862fb8b8
 def check_owner(owner_id):
     for i in users.find({}):
         if str(i["_id"]) == owner_id:
             return 1
-<<<<<<< HEAD
 
     return 0
 
@@ -156,22 +106,6 @@ def add_owner(device_id, owner_id):
     if check_owner(owner_id) == 0:
         return {"code": 404, "message": "User not found"}
 
-=======
-    
-    return 0
-
-def check_device(device_id):
-    for i in devices.find({}):
-        if i["id"] == device_id and i["owner_id"] == None:
-            return 1
-    
-    return 0
-
-def add_owner(device_id, owner_id):
-    if check_owner(owner_id) == 0:
-        return {"code": 404, "message": "User not found"}
-    
->>>>>>> 05fcb1cfc319d7c78797c18676d155fa862fb8b8
     if check_device(device_id) == 0:
         return {"code": 404, "message": "Device not found or already has an owner"}
 
@@ -184,20 +118,13 @@ def add_owner(device_id, owner_id):
         new_array = []
 
     new_array.append(device_id)
-<<<<<<< HEAD
     values = {"$set": {"owner_id": owner_id}}
 
     users.update_one(owner_schema, {"$set": {"device_ids": new_array}})
-=======
-    values = { "$set": { "owner_id": owner_id } }
-
-    users.update_one(owner_schema, { "$set": { "device_ids": new_array } } )    
->>>>>>> 05fcb1cfc319d7c78797c18676d155fa862fb8b8
     devices.update_one(device_schema, values)
 
     return {"code": 200, "message": "Owner added successfully"}
 
-<<<<<<< HEAD
 
 def add_break_in(device_id):
     if check_device(device_id, False) == 0:
@@ -233,5 +160,3 @@ def get_break_ins(device_id):
         index += 1
 
     return {"code": 200, "break_ins": breaks}
-=======
->>>>>>> 05fcb1cfc319d7c78797c18676d155fa862fb8b8
